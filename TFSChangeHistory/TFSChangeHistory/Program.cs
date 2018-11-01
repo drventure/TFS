58d3e5cc896aafb48933ca35572ab66b4a687f97
Darin Higgins <dhiggins@worksoft.com>
2018-11-01 12:48:56 -0500

    Didn't quite have null command line args working right.

--- TFSChangeHistory/TFSChangeHistory/Program.cs
+++ TFSChangeHistory/TFSChangeHistory/Program.cs
@@ -21,11 +21,11 @@ namespace TFSChangeHistory
         [STAThread]
         static void Main(string[] args)
         {
-            if (args == null)
+            if (args.Length == 0)
             {
                 Application.EnableVisualStyles();
                 Application.SetCompatibleTextRenderingDefault(false);
-                Application.Run(new Form1());
+				Application.Run(new Form1());
             }
             else
             {
